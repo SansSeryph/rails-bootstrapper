8d27eb5c3dc73af1607ffcf9d9b3e4181529e511
SansSeryph <sansseryph@gmail.com>
2022-06-20 20:13:25 -0700

    Add post-install messages for follow-up items

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Starting!"
+
 ################################################################################
 ## Get the latest versions of everything
 ################################################################################
@@ -21,7 +23,6 @@ asdf reshim nodejs
 cd ~/Workspace
 rails new --css=sass podcast-site
 cd podcast-site
-# TODO: clean up Gemfile
 git add .
 git commit -m "Initialize new Rails project"
 
@@ -31,7 +32,6 @@ git commit -m "Initialize new Rails project"
 
 bundle add --group=development --require=false rubocop-rails \
   rubocop-performance rubocop-rake
-# TODO Move installed gems to right spot
 ln ~/Workspace/rails-bootstrapper/rubocop.yml \
   ~/Workspace/podcast-site/.rubocop.yml
 bundle exec rubocop --autocorrect
@@ -46,7 +46,6 @@ git commit -m "Add and configure Rubocop"
 
 # RSpec
 bundle add --group=development,test rspec-rails rubocop-rspec
-# TODO Move installed gems to right spot
 bundle exec rails generate rspec:install
 # TODO: make updates around rubocop.yml
 #       - move rspec rules to separate file (including require line)
@@ -58,7 +57,6 @@ git commmit -m "Add and configure RSpec"
 
 # Factory Bot
 bundle add --group=development,test factory_bot_rails
-# TODO: move gem line to the right spot
 mkdir -p spec/support/
 ln ~/Workspace/rails-bootstrapper/factory_bot_rails.rb spec/support/
 # TODO: cli command to include the factory bot file in `spec/rails_helper.rb`
@@ -82,7 +80,6 @@ git commit -m "Add HAML gem and convert ERB files to HAML"
 # TODO: add route/controller/view for home? so that the root route below works
 
 bundle add devise
-# TODO: clean up Gemfile
 bundle exec rails generate devise:install
 # TODO: add the following line to `config/environments/development.rb`:
 # `config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }`
@@ -102,3 +99,12 @@ rm spec/models/user_spec.rb
 bundle exec rubocop --autocorrect
 git add .
 git commit -m "Add and configure Devise"
+
+################################################################################
+## Post-Install Message
+################################################################################
+
+echo "Project Setup Complete!"
+echo "You may want to do a few things manually:"
+echo "- Clean up Gemfile"
+echo "- Configure Devise"
